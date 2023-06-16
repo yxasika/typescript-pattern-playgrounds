9728b547689c14ae2ac0a08b0eb5b0ed4c28b2b5
Sven Goetz <xztaxdltalw7swacfa6qnqysozzq5ifd3lwa3fdi7q3abdqmit5a>
2023-06-16 10:04:18 +0200

    MASTER/000000 syntax-bugfix factory and cleanup.

--- factory/factory.ts
+++ factory/factory.ts
@@ -1,17 +1,17 @@
 
 export abstract class HTMLElement {
     protected abstract selector: string;
-    
+
     private readonly nodeList: HTMLElement[] = [];
-    
+
     printNodes(): string {
         return `${this.selector}: [${this.getNodes().map(n => n.printNodes()).join(', ')}]`;
     }
-    
+
     public addNode(...htmlElementNode: HTMLElement[]): void {
         this.nodeList.push(...htmlElementNode);
     }
-    
+
     public getNodes(): HTMLElement[] {
         return this.nodeList;
     }
@@ -19,9 +19,9 @@ export abstract class HTMLElement {
 
 export class DivElement extends HTMLElement {
     protected selector = 'div';
-    
+
     // Unused style-property as an example for extensions of factory-functions.
-    constructor(private readonly styles?: string) {
+    constructor(private readonly _styles?: string) {
         super();
     }
 }
@@ -32,14 +32,14 @@ export class SpanElement extends HTMLElement {
 
 export class H1Element extends HTMLElement {
     protected selector = 'h1';
-    constructor(private readonly innerText?: string) {
+    constructor(private readonly _innerText?: string) {
         super();
     }
 }
 
 export class H2Element extends HTMLElement {
     protected selector = 'h2';
-    constructor(private readonly innerText?: string) {
+    constructor(private readonly _innerText?: string) {
         super();
     }
 }
@@ -50,32 +50,32 @@ export class H2Element extends HTMLElement {
  */
 export class HtmlElementBuilder {
     private tail;
-    
+
     constructor(private readonly rootElement: HTMLElement) {
         this.tail = this.rootElement;
     }
-    
+
     createRoot(): HTMLElement {
         return this.rootElement;
     }
-    
+
     appendNode<T extends { new (...args: ConstructorParameters<T>): HTMLElement }>(type: T, ...params: ConstructorParameters<T>): HtmlElementBuilder {
         const leaf = HtmlElementBuilder.createHtmlElement(type, ...params);
         this.tail.addNode(leaf);
         this.tail = leaf;
         return this;
     }
-    
-    insertNode(...elements: HTMLElement): HtmlElementBuilder {
+
+    insertNode(...elements: HTMLElement[]): HtmlElementBuilder {
         this.tail.addNode(...elements);
         return this;
     }
-    
+
     static createHtmlDom<T extends { new (...args: ConstructorParameters<T>): HTMLElement }>(type: T, ...params: ConstructorParameters<T>): HtmlElementBuilder {
         const rootNode = HtmlElementBuilder.createHtmlElement(type, ...params);
         return new HtmlElementBuilder(rootNode);
     }
-    
+
     private static createHtmlElement<T extends { new (...args: ConstructorParameters<T>): HTMLElement }>(type: T, ...params: ConstructorParameters<T>): HTMLElement {
         return new type(...params);
     }
